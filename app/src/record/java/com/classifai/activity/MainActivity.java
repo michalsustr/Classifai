db15c72956eb7df40b3cda764b67280034e10608
michal.sustr <michal.sustr@gmail.com>
2016-04-22 16:30:43 +0200

    information based on label

--- app/src/record/java/com/classifai/activity/MainActivity.java
+++ app/src/record/java/com/classifai/activity/MainActivity.java
@@ -1,13 +1,25 @@
 package com.classifai.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.classifai.R;
 import com.classifai.tools.camera.Camera;
 import com.classifai.tools.camera.CroppedCameraPreview;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,10 +27,22 @@ import java.util.TimerTask;
 public class MainActivity extends Activity {
     private static final String TAG = "classifai";
 
-    private CroppedCameraPreview cameraPreview;
-    private Camera camera;
+    private static final String SAVE_DIR  = "/storage/sdcard0/caffe/record/";
 
+    private CroppedCameraPreview cameraPreview;
+    private AutoCompleteTextView labelText;
+    private TextView capturedFrames;
+    private Button recordBtn;
+
+    private Camera camera;
     private boolean isResumed = false;
+
+    private ArrayAdapter<String> suggestionsAdapter;
+    private int numFrames = 0;
+    private int numRecordings = 0;
+    private boolean isRecording = false;
+    private CharSequence labelName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +52,108 @@ public class MainActivity extends Activity {
 
         cameraPreview = (CroppedCameraPreview) findViewById(R.id.preview_surface);
         camera = new Camera(this, cameraPreview);
+        labelText = (AutoCompleteTextView) findViewById(R.id.labelText);
+        labelText.setThreshold(0);
+        capturedFrames = (TextView) findViewById(R.id.capturedFrames);
+        recordBtn = (Button) findViewById(R.id.recordBtn);
+
+        suggestionsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getSuggestions());
+        labelText.setAdapter(suggestionsAdapter);
+        labelText.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                hideKeyboard();
+                MainActivity.this.onLabelTextChanged(suggestionsAdapter.getItem(position));
+            }
+            @Override public void onNothingSelected(AdapterView<?> parent) {}
+        });
+        labelText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecording();
+            }
+        });
+        labelText.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.this.onLabelTextChanged(s);
+            }
+            @Override public void afterTextChanged(Editable s) {}
+        });
+        labelText.setText(suggestionsAdapter.getItem(0));
     }
+
+    private String[] getSuggestions() {
+        File dir = new File(SAVE_DIR);
+        FileFilter fileFilter = new FileFilter() {
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        };
+        File[] dirs = dir.listFiles(fileFilter);
+        String[] staticSuggestions = getResources().getStringArray(R.array.label_suggestions);
+        String[] suggestions = new String[staticSuggestions.length + dirs.length];
+        for (int i = 0; i < dirs.length; i++) {
+            suggestions[i] = dirs[i].getName();
+        }
+        for (int i = dirs.length; i < staticSuggestions.length + dirs.length; i++) {
+            suggestions[i] = staticSuggestions[i-dirs.length];
+        }
+        return suggestions;
+    }
+
+    public void onLabelTextChanged(CharSequence s) {
+        Log.d(TAG, "onLabelTextChanged "+s);
+        // if is recording, stop
+        if(isRecording) {
+            stopRecording();
+        }
+        labelName = s;
+        numFrames = 0;
+
+        // try to find folder and info
+        File candidateDir = new File(SAVE_DIR +"/"+s);
+        if(!candidateDir.exists() || !candidateDir.isDirectory()) {
+            numRecordings = 0;
+        } else {
+            numRecordings = candidateDir.listFiles().length;
+        }
+        updateInfo();
+    }
+
+    public void onRecordClick(View view) {
+        if(isRecording) {
+            stopRecording();
+        } else {
+            startRecording();
+        }
+    }
+
+    private void startRecording() {
+        isRecording = true;
+        recordBtn.setText(getString(R.string.stop_recording));
+        recordBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.stop, 0, 0, 0);
+    }
+
+    private void stopRecording() {
+        isRecording = false;
+        recordBtn.setText(getString(R.string.start_recording));
+        recordBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.start, 0,0,0);
+    }
+
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void updateInfo() {
+        capturedFrames.setText(String.format("%d frames\n%d recordings", numFrames, numRecordings));
+    }
+
 
 
 
@@ -62,6 +187,9 @@ public class MainActivity extends Activity {
             }, 500);
         }
 
+        // set initial state correctly
+        stopRecording();
+
         super.onPostResume();
     }
 
@@ -69,8 +197,8 @@ public class MainActivity extends Activity {
     protected void onPause() {
         Log.d(TAG, "MainActivity.onPause");
         super.onPause();
+        stopRecording();
         camera.closeCamera();
         isResumed = false;
     }
-
 }
